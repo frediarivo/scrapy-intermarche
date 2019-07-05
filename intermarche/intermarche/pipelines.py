ffc8939a3fa7bcecf9b90285875edcca1d3564b2
Fred Hermann R <47384414+frediarivo@users.noreply.github.com>
2019-07-05 13:07:53 +0300

    Uupdate pipeline.py
    Amlioration de la génération de l'id de tous les spiders (item)

--- intermarche/intermarche/pipelines.py
+++ intermarche/intermarche/pipelines.py
@@ -5,14 +5,10 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-
 class IntermarchePipeline(object):
 	counter_id = 0
-
 	def process_item(self, item, spider):
-		if spider.name == 'spider_category':
-			self.counter_id += 1
-			item['id'] = self.counter_id
-            return item
-        
+		self.counter_id += 1
+		item['id'] = str(self.counter_id)
+		
 		return item
